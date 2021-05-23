a92e847a0daa31474fdf58606c5bb809d9b8b9e8
Shreeshail G <shreeshailgumgeri@gmail.com>
2021-05-23 23:31:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -14,30 +14,6 @@ data "aws_ami" "amazon_linux" {
   owners = ["amazon"]
 }
 
-resource "aws_key_pair" "developer" {
-  public_key = var.public_key
-}
-
-resource "aws_instance" "machine" {
-  ami = data.aws_ami.amazon_linux.id
-  instance_type = "t3.micro"
-  iam_instance_profile = aws_iam_instance_profile.machine.id
-  key_name = aws_key_pair.developer.key_name
-  associate_public_ip_address = true
-
-  root_block_device {
-    volume_size = 20
-    volume_type = "gp2"
-  }
-
-  tags = {
-    Name = local.system_name
-  }
-
-  vpc_security_group_ids = [aws_security_group.machine_access.id]
-  user_data_base64 = data.cloudinit_config.init.rendered
-}
-
 resource "aws_security_group" "machine_access" {
   name_prefix = local.system_name
 
@@ -58,6 +34,30 @@ resource "aws_security_group" "machine_access" {
       "0.0.0.0/0"]
   }
 }
+
+resource "aws_instance" "machine" {
+  ami = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.machine.id
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+
+  tags = {
+    Name = local.system_name
+  }
+
+  vpc_security_group_ids = [aws_security_group.machine_access.id]
+  
+  key_name = "us-east-key"
+  
+  user_data_base64 = data.cloudinit_config.init.rendered
+}
+
+
 
 data "cloudinit_config" "init" {
   part {
