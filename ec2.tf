bfd06bb7515f42510cacd6a28e75f8d7ce268c4e
Shreeshail G <shreeshailgumgeri@gmail.com>
2021-05-23 16:54:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,11 +2,6 @@ data "aws_ami" "amazon_linux" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0*"]
-  }
-
-  filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
